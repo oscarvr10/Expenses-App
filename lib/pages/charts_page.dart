1c251b966bb41aaded29e48ad3d5b695b6a7c693
overa92 <84212787+overa92@users.noreply.github.com>
2023-04-09 21:31:41 -0600

    Creacion de grafico lineal de gastos

--- lib/pages/charts_page.dart
+++ lib/pages/charts_page.dart
@@ -1,12 +1,52 @@
+import 'package:exp_app/widgets/charts_page_wt/chart_line.dart';
 import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
 
 class ChartsPage extends StatelessWidget {
   const ChartsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Charts Page'),
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColorDark,
+      appBar: AppBar(
+        title: const Text('Gráfico'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 350.0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text('Selector'),
+                    Expanded(
+                      child: ChartLine(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.only(top: 10.0),
+              height: 40.0,
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: Container(
+                decoration: Constants.sheetBoxDecoration(
+                    Theme.of(context).primaryColorDark),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,5 +1,15 @@
 import 'package:exp_app/utils/icon_list.dart';
 import 'package:flutter/material.dart';
+export 'package:exp_app/utils/extensions.dart';
+
+int daysInMonth(int month) {
+  var currentDate = DateTime.now();
+  var lastDay = month < 12
+      ? DateTime(currentDate.year, month + 1, 0)
+      : DateTime(currentDate.year + 1, 1, 0);
+
+  return lastDay.day;
+}
 
 extension ColorExtension on String {
   toColor() {
